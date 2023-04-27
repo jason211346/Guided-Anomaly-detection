09a82fbf4e6aaf78c87344154efaf167e47214d1
jason211346 <jason211346@gmail.com>
2023-04-27 13:28:07 +0800

    update

--- PCB_dataset/train.sh
+++ PCB_dataset/train.sh
@@ -1,11 +1,10 @@
 # data regroup
 
+CUDA_VISIBLE_DEVICES=0 python train_data_regroup.py --output_dir /root/notebooks/nfs/work/jason.chen/DUE/regroup_output_model/phison_data_regroup --seed 1212
 
 for seed in 2 211 1212
 do 
-    
     CUDA_VISIBLE_DEVICES=0 python train_CEdue.py --output_dir /root/notebooks/nfs/work/jason.chen/DUE/regroup_output_model/CEdue --seed $seed
-    
 done
 
 
@@ -13,7 +12,5 @@ done
 for seed in 2 211 1212
 do 
     ckpt=/root/notebooks/nfs/work/jason.chen/DUE/regroup_output_model/CEdue/$seed/ckpt.pt
-    
     CUDA_VISIBLE_DEVICES=0 python train_CEDUE_stage2.py --output_dir /root/notebooks/nfs/work/jason.chen/DUE/regroup_output_model/CEdue_s2 --checkpoint_path $ckpt --seed $seed
-    
 done
